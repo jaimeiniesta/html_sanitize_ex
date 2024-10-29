6ddd1bfd387348f5ba98ae253d5076806e2139d1
Jaime Iniesta <github@jaimeiniesta.com>
2024-10-29 13:18:42 +0100

    adds failing test for issue #62

--- test/custom_scrubber_test.exs
+++ test/custom_scrubber_test.exs
@@ -11,6 +11,7 @@ defmodule CustomScrubberTest do
     Meta.strip_comments()
 
     Meta.allow_tag_with_any_attributes("p")
+    Meta.allow_tag_with_uri_attributes("a", ["href"], ["https", "mailto"])
 
     Meta.allow_tags_with_style_attributes(["span", "html", "body"])
 
@@ -23,9 +24,9 @@ defmodule CustomScrubberTest do
 
   test "strips everything except the allowed tags (for multiple tags)" do
     input =
-      "<section><header><script>code!</script></header><p>hello <script>code!</script></p></section>"
+      "<section><header><script>code!</script></header><p>hello <script>code!</script><a href=\"https://example.com\">link</a></p></section>"
 
-    expected = "code!<p>hello code!</p>"
+    expected = "code!<p>hello code!<a href=\"https://example.com\">link</a></p>"
     assert expected == scrub(input)
   end
 end
